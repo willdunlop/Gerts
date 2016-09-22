2f5065eb742963b99254251bbb012acb4e27b3e0
willdunlop <william.dunlop.1993@gmail.com>
2016-09-22 14:04:23 +1000

    nothing really

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
   resources :books do
     member do
-
-      put "voteone", to: "books#voteone"
-      put "votetwo", to: "books#votetwo"
-      put "votethree", to: "books#votethree"
-      put "votefour", to: "books#votefour"
-      put "votefive", to: "books#votefive"
-      put "unvote", to: "books#unvote"
+      put 'voteone', to: 'books#voteone'
+      put 'votetwo', to: 'books#votetwo'
+      put 'votethree', to: 'books#votethree'
+      put 'votefour', to: 'books#votefour'
+      put 'votefive', to: 'books#votefive'
+      put 'unvote', to: 'books#unvote'
+    end
   end
-end
-
   devise_for :users
-
   root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
